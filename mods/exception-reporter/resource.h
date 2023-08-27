f4b33257c02a8d85f49ca077adfe712818c8d43d
Matty <54020396+matty-ross@users.noreply.github.com>
2023-08-27 14:19:37 +0200

    Added a dialog resource.

---
+++ mods/exception-reporter/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by exception-reporter.rc
+//
+#define IDD_DIALOG_EXCEPTION_REPORT     101
+#define IDC_GROUP_GENERAL_INFORMATION   1001
+#define IDC_TEXT_CODE                   1002
+#define IDC_VALUE_CODE                  1003
+#define IDC_TEXT_ADDRESS                1004
+#define IDC_VALUE_ADDRESS               1005
+#define IDC_TEXT_FLAGS                  1006
+#define IDC_VALUE_FLAGS                 1007
+#define IDC_GROUP_PARAMETERS            1008
+#define IDC_VALUE_PARAMETERS            1009
+#define IDC_GROUP_REGISTERS             1010
+#define IDC_VALUE_REGISTERS             1011
+#define IDC_GROUP_STACK_TRACE           1012
+#define IDC_VALUE_STACK_TRACE           1013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
