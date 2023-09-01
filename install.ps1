d31742a277dd779e899131e386131635b7fcd8e5
Matty <54020396+matty-ross@users.noreply.github.com>
2023-09-01 15:08:28 +0200

    Added an installation script.

---
+++ install.ps1
@@ -0,0 +1,8 @@
+﻿$Configuration = "Debug x86"
+#$Configuration = "Release x86"
+
+$BprDirectory = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\WOW6432Node\Criterion\BurnoutPR\" -Name "Install Dir"
+
+Copy-Item -Path ".\other\mod-manager\bin\$Configuration\mod-manager.dll" -Destination $BprDirectory
+Copy-Item -Path ".\vendor\imgui\bin\$Configuration\imgui.dll" -Destination $BprDirectory
+Copy-Item -Path ".\mods\*\bin\$Configuration\*.dll" -Destination ($BprDirectory + "mods\")
