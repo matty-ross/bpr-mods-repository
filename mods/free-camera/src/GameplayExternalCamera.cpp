1d78348ce5dadeb808d2a57e30d06c1e07f44c27
Matty <54020396+matty-ross@users.noreply.github.com>
2023-09-03 20:42:16 +0200

    [FreeCamera] Refactored parameters.

--- mods/free-camera/src/GameplayExternalCamera.cpp
+++ mods/free-camera/src/GameplayExternalCamera.cpp
@@ -3,44 +3,64 @@
 #include "imgui/imgui.h"
 
 
+GameplayExternalCamera::GameplayExternalCamera()
+    :
+    m_Parameters
+    {
+        .PitchSpring              = 0x3C,
+        .YawSpring                = 0x40,
+        .PivotY                   = 0x4C,
+        .PivotZ                   = 0x50,
+        .PivotZOffset             = 0x54,
+        .FOV                      = 0x6C,
+        .InFrontFOVMax            = 0x70,
+        .FrontInAmount            = 0x74,
+        .DriftYawSpring           = 0x8C,
+        .BoostFOVZoomCompensation = 0x90,
+        .DownAngle                = 0x94,
+        .DropFactor               = 0xA8,
+    }
+{
+}
+
 void GameplayExternalCamera::OnUpdate(Core::Pointer camera, Core::Pointer sharedInfo)
 {
-    auto updateParameter = [this](Parameter& parameter, Core::Pointer address) -> void
-    {
-        if (m_UpdateDefaultValues)
-        {
-            parameter.DefaultValue = address.as<float>();
-        }
-
-        if (parameter.Reset || m_ResetAllParameters)
-        {
-            address.as<float>() = parameter.DefaultValue;
-            parameter.Reset = false;
-        }
-    };
-
-
     uint64_t attribKey = Core::Pointer(0x0013FC8E0).deref().at(0x7165C0).as<uint64_t>();
-    m_UpdateDefaultValues = attribKey != m_PreviousAttribKey;
 
     {
-        Core::Pointer parameters = Core::Pointer(0x0013FC8E0).deref().at(0x7165C8);
+        auto updateParameter = [this](Parameter& parameter, Core::Pointer parameters, bool updateDefaultParameterValues) -> void
+        {
+            if (updateDefaultParameterValues)
+            {
+                parameter.DefaultValue = parameters.at(parameter.Offset).as<float>();
+            }
 
-        updateParameter(m_PitchSpring, parameters.at(0x3C));
-        updateParameter(m_YawSpring, parameters.at(0x40));
-        updateParameter(m_PivotY, parameters.at(0x4C));
-        updateParameter(m_PivotZ, parameters.at(0x50));
-        updateParameter(m_PivotZOffset, parameters.at(0x54));
-        updateParameter(m_FOV, parameters.at(0x6C));
-        updateParameter(m_InFrontFOVMax, parameters.at(0x70));
-        updateParameter(m_FrontInAmount, parameters.at(0x74));
-        updateParameter(m_DriftYawSpring, parameters.at(0x8C));
-        updateParameter(m_BoostFOVZoomCompensation, parameters.at(0x90));
-        updateParameter(m_DownAngle, parameters.at(0x94));
-        updateParameter(m_DropFactor, parameters.at(0xA8));
+            if (parameter.Reset || m_ResetAllParameters)
+            {
+                parameters.at(parameter.Offset).as<float>() = parameter.DefaultValue;
+                parameter.Reset = false;
+            }
+        };
+
+        Core::Pointer parameters = Core::Pointer(0x0013FC8E0).deref().at(0x7165C8);
+        bool updateDefaultParameterValues = attribKey != m_PreviousAttribKey;
+        
+        updateParameter(m_Parameters.PitchSpring, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.YawSpring, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.PivotY, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.PivotZ, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.PivotZOffset, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.FOV, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.InFrontFOVMax, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.FrontInAmount, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.DriftYawSpring, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.BoostFOVZoomCompensation, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.DownAngle, parameters, updateDefaultParameterValues);
+        updateParameter(m_Parameters.DropFactor, parameters, updateDefaultParameterValues);
+        
+        m_ResetAllParameters = false;
     }
 
-    m_ResetAllParameters = false;
     m_PreviousAttribKey = attribKey;
 }
 
@@ -51,7 +71,7 @@ void GameplayExternalCamera::OnRenderMenu()
         if (ImGui::TreeNode("Parameters"))
         {
             using WidgetFn = void(*)(Core::Pointer);
-            auto renderParameter = [](Parameter& parameter, Core::Pointer address, WidgetFn widget) -> void
+            auto renderParameter = [](Parameter& parameter, Core::Pointer parameters, WidgetFn widget) -> void
             {
                 ImGui::PushID(&parameter);
                 if (ImGui::Button("Reset"))
@@ -59,10 +79,9 @@ void GameplayExternalCamera::OnRenderMenu()
                     parameter.Reset = true;
                 }
                 ImGui::SameLine();
-                widget(address);
+                widget(parameters.at(parameter.Offset));
                 ImGui::PopID();
             };
-
 
             Core::Pointer parameters = Core::Pointer(0x0013FC8E0).deref().at(0x7165C8);
 
@@ -71,73 +90,73 @@ void GameplayExternalCamera::OnRenderMenu()
                 m_ResetAllParameters = true;
             }
 
-            renderParameter(m_PitchSpring, parameters.at(0x3C),
+            renderParameter(m_Parameters.PitchSpring, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("Pitch Spring", &address.as<float>(), 0.0f, 1.0f);
                 }
             );
-            renderParameter(m_YawSpring, parameters.at(0x40),
+            renderParameter(m_Parameters.YawSpring, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("Yaw Spring", &address.as<float>(), 0.0f, 1.0f);
                 }
             );
-            renderParameter(m_DriftYawSpring, parameters.at(0x8C),
+            renderParameter(m_Parameters.DriftYawSpring, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("Drift Yaw Spring", &address.as<float>(), 0.0f, 1.0f);
                 }
             );
-            renderParameter(m_PivotY, parameters.at(0x4C),
+            renderParameter(m_Parameters.PivotY, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("Pivot Y", &address.as<float>(), 0.0f, 50.0f);
                 }
             );
-            renderParameter(m_PivotZ, parameters.at(0x50),
+            renderParameter(m_Parameters.PivotZ, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("Pivot Z", &address.as<float>(), 0.0f, 50.0f);
                 }
             );
-            renderParameter(m_PivotZOffset, parameters.at(0x54),
+            renderParameter(m_Parameters.PivotZOffset, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("Pivot Z Offset", &address.as<float>(), 0.0f, 50.0f);
                 }
             );
-            renderParameter(m_FOV, parameters.at(0x6C),
+            renderParameter(m_Parameters.FOV, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("FOV", &address.as<float>(), 1.0f, 179.0f);
                 }
             );
-            renderParameter(m_InFrontFOVMax, parameters.at(0x70),
+            renderParameter(m_Parameters.InFrontFOVMax, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("In Front FOV Max", &address.as<float>(), 1.0f, 179.0f);
                 }
             );
-            renderParameter(m_FrontInAmount, parameters.at(0x74),
+            renderParameter(m_Parameters.FrontInAmount, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("Front In Amount", &address.as<float>(), -2.0f, 2.0f);
                 }
             );
-            renderParameter(m_BoostFOVZoomCompensation, parameters.at(0x90),
+            renderParameter(m_Parameters.BoostFOVZoomCompensation, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("Boost FOV Zoom Compensation", &address.as<float>(), -5.0f, 5.0f);
                 }
             );
-            renderParameter(m_DownAngle, parameters.at(0x94),
+            renderParameter(m_Parameters.DownAngle, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("Down Angle", &address.as<float>(), 0.0f, 90.0f);
                 }
             );
-            renderParameter(m_DropFactor, parameters.at(0xA8),
+            renderParameter(m_Parameters.DropFactor, parameters,
                 [](Core::Pointer address) -> void
                 {
                     ImGui::SliderFloat("Drop Factor", &address.as<float>(), 0.0f, 1.0f);
--- mods/free-camera/src/GameplayExternalCamera.h
+++ mods/free-camera/src/GameplayExternalCamera.h
@@ -9,32 +9,42 @@
 class GameplayExternalCamera
 {
 public:
+    GameplayExternalCamera();
+
+public:
     void OnUpdate(Core::Pointer camera, Core::Pointer sharedInfo);
     void OnRenderMenu();
 
 private:
     struct Parameter
     {
-        float DefaultValue;
-        bool Reset;
+        ptrdiff_t Offset;
+        float DefaultValue = 0.0f;
+        bool Reset = false;
+
+        Parameter(ptrdiff_t offset)
+        {
+            Offset = offset;
+        }
     };
 
 private:
     uint64_t m_PreviousAttribKey = 0x0000000000000000;
 
-    Parameter m_PitchSpring = {};
-    Parameter m_YawSpring = {};
-    Parameter m_PivotY = {};
-    Parameter m_PivotZ = {};
-    Parameter m_PivotZOffset = {};
-    Parameter m_FOV = {};
-    Parameter m_InFrontFOVMax = {};
-    Parameter m_FrontInAmount = {};
-    Parameter m_DriftYawSpring = {};
-    Parameter m_BoostFOVZoomCompensation = {};
-    Parameter m_DownAngle = {};
-    Parameter m_DropFactor = {};
-
-    bool m_UpdateDefaultValues = false;
+    struct
+    {
+        Parameter PitchSpring;
+        Parameter YawSpring;
+        Parameter PivotY;
+        Parameter PivotZ;
+        Parameter PivotZOffset;
+        Parameter FOV;
+        Parameter InFrontFOVMax;
+        Parameter FrontInAmount;
+        Parameter DriftYawSpring;
+        Parameter BoostFOVZoomCompensation;
+        Parameter DownAngle;
+        Parameter DropFactor;
+    } m_Parameters;
     bool m_ResetAllParameters = false;
 };
