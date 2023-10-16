5772e191d7fc8b7d966cab282540a2284e659bd8
Matty <54020396+matty-ross@users.noreply.github.com>
2023-10-16 20:28:51 +0200

    [Protection] Boilerplate saving file code.

--- mods/protection/src/VehiclesFile.cpp
+++ mods/protection/src/VehiclesFile.cpp
@@ -1,5 +1,11 @@
 #include "VehiclesFile.h"
 
+#include <Windows.h>
+
+#include "yaml-cpp/yaml.h"
+
+#include "core/File.h"
+
 
 VehiclesFile::VehiclesFile(const Core::Logger& logger, const std::string& filePath)
     :
@@ -14,6 +20,47 @@ void VehiclesFile::Load()
 
 void VehiclesFile::Save()
 {
+    try
+    {
+        m_Logger.Info("Saving vehicles to file '%s' ...", m_FilePath.c_str());
+
+        auto writeFile = [this](const std::string& content) -> void
+        {
+            try
+            {
+                Core::File file(m_FilePath, GENERIC_WRITE, FILE_SHARE_READ, CREATE_ALWAYS);
+                file.Write(content);
+            }
+            catch (const std::runtime_error& e)
+            {
+                m_Logger.Warning("%s. Last error: 0x%08X.", e.what(), GetLastError());
+            }
+        };
+
+        // TODO: use the uncompressed vehicle IDss
+
+        YAML::Node yaml;
+        {
+            yaml["FallbackVehicleID"] = m_FallbackVehicleID;
+        }
+
+        for (uint64_t vehicleID : m_VehicleIDs)
+        {
+            uint64_t replacementVehicleID = m_Vehicles.at(vehicleID);
+            
+            char key[32] = {};
+            sprintf_s(key, "%llu", replacementVehicleID);
+            yaml["Vehicles"][key] = replacementVehicleID;
+        }
+
+        writeFile(YAML::Dump(yaml));
+
+        m_Logger.Info("Saved vehicles.");
+    }
+    catch (const std::exception& e)
+    {
+        m_Logger.Warning("Failed to save vehicles. %s", e.what());
+    }
 }
 
 uint64_t VehiclesFile::GetFallbackVehicleID() const
