3ed63b6476b1d621445ef25a7d7778b26d309d7b
Matty <54020396+matty-ross@users.noreply.github.com>
2024-04-28 14:09:49 +0200

    Added defines for easier use of vendor libraries.

--- vendor/imgui/include/vendor/imgui.hpp
+++ vendor/imgui/include/vendor/imgui.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 
+#define IMGUI_API __declspec(dllimport)
+
+
 #include "../../modules/imgui/imgui.h"
 #include "../../modules/imgui/backends/imgui_impl_win32.h"
 #include "../../modules/imgui/backends/imgui_impl_dx11.h"
--- vendor/yaml-cpp/include/vendor/yaml-cpp.hpp
+++ vendor/yaml-cpp/include/vendor/yaml-cpp.hpp
@@ -1,4 +1,7 @@
 #pragma once
 
 
+#define YAML_CPP_STATIC_DEFINE
+
+
 #include "../../modules/yaml-cpp/include/yaml-cpp/yaml.h"
