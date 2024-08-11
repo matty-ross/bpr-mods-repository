308a4153288ac01f948bf251206391d7736ac74f
Matty <54020396+matty-ross@users.noreply.github.com>
2024-08-11 20:13:03 +0200

    Updated install script.

--- install.ps1
+++ install.ps1
@@ -1,26 +1,23 @@
-function Get-Configuration
+$Configurations = (
+    "Debug x86",
+    "Release x86"
+)
+
+Write-Host -Object "Choose configuration:"
+for ($i = 0; $i -lt $Configurations.Length; ++$i)
 {
-    $Configurations = (
-        "Debug x86",
-        "Release x86"
-    )
-    
-    $Prompt = "Choose configuration:`n"
-    for ($i = 0; $i -lt $Configurations.Length; ++$i)
-    {
-        $Prompt += "$i. " + $Configurations[$i] + "`n";
-    }
-    $Choice = Read-Host -Prompt $Prompt
-    
-    return $Configurations[$Choice]
+    Write-Host -Object "$($i + 1) - $($Configurations[$i])"
 }
 
+$Choice = Read-Host -Prompt ">"
+$Configuration = $Configurations[$Choice - 1]
 
-$Configuration = Get-Configuration
 $BprDirectory = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\WOW6432Node\Criterion\BurnoutPR\" -Name "Install Dir"
+$BprModsDirectory = $BprDirectory + "mods\"
 
 Copy-Item -Path ".\libraries\mod-manager\bin\$Configuration\mod-manager.dll" -Destination $BprDirectory
 Copy-Item -Path ".\vendor\imgui\bin\$Configuration\imgui.dll" -Destination $BprDirectory
-Copy-Item -Path ".\mods\*\bin\$Configuration\*.dll" -Destination ($BprDirectory + "mods\")
+Copy-Item -Path ".\mods\*\bin\$Configuration\*.dll" -Destination $BprModsDirectory
 
 Write-Host -Object "Installation finished."
+Read-Host -Prompt "Press Enter to quit..." | Out-Null
