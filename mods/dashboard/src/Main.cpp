7adcd5346d68c40a6ca7eea31094fd7ef6afd726
Matty <54020396+matty-ross@users.noreply.github.com>
2024-12-14 20:16:20 +0100

    New mod: Dashboard

---
+++ mods/dashboard/src/Main.cpp
@@ -0,0 +1,20 @@
+#include <Windows.h>
+
+
+BOOL WINAPI DllMain(
+    _In_ HINSTANCE hinstDLL,
+    _In_ DWORD     fdwReason,
+    _In_ LPVOID    lpvReserved
+)
+{
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        break;
+
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+
+    return TRUE;
+}
