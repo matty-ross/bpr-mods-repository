e96826e482720ff94dc52a6131e44a995db6546d
Matty <54020396+matty-ross@users.noreply.github.com>
2025-10-26 20:55:26 +0100

    [Core] Removed filesystem and added path helper class.

---
+++ libraries/core/include/core/Path.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+
+#include <Windows.h>
+
+
+namespace Core
+{
+    class Path
+    {
+    public:
+        Path(const char* path);
+
+    public:
+        const char* GetPath() const;
+
+        void Append(const char* path);
+        void Create() const;
+
+    private:
+        char m_Path[MAX_PATH] = {};
+    };
+}
---
+++ libraries/core/src/Path.cpp
@@ -0,0 +1,35 @@
+#include "Path.hpp"
+
+#include <Shlwapi.h>
+#include <ShlObj.h>
+
+#include "WindowsException.hpp"
+
+
+namespace Core
+{
+    Path::Path(const char* path)
+    {
+        ExpandEnvironmentStringsA(path, m_Path, sizeof(m_Path));
+    }
+
+    const char* Path::GetPath() const
+    {
+        return m_Path;
+    }
+
+    void Path::Append(const char* path)
+    {
+        PathAppendA(m_Path, path);
+    }
+    
+    void Path::Create() const
+    {
+        int result = SHCreateDirectoryExA(NULL, m_Path, nullptr);
+        if (result != ERROR_SUCCESS && result != ERROR_ALREADY_EXISTS)
+        {
+            SetLastError(result);
+            throw WindowsException("Cannot create directory");
+        }
+    }
+}
