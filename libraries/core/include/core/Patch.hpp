6745315378dd352e9f474b1a7cb6162fd4dfd81c
Matty <54020396+matty-ross@users.noreply.github.com>
2025-12-24 20:27:14 +0100

    [Core] Implemented the Patch class.

--- libraries/core/include/core/Patch.hpp
+++ libraries/core/include/core/Patch.hpp
@@ -1,19 +1,25 @@
 #pragma once
 
 
+#include <cstdint>
+
+
 namespace Core
 {
     class Patch
     {
     public:
         Patch(void* address, size_t size);
+        ~Patch();
 
     public:
-        void Apply();
+        void Apply(const void* bytes);
         void Remove();
 
     private:
         void* m_Address = nullptr;
         size_t m_Size = 0;
+
+        uint8_t* m_OriginalBytes = nullptr;
     };
 }
--- libraries/core/src/Patch.cpp
+++ libraries/core/src/Patch.cpp
@@ -1,5 +1,8 @@
 #include "Patch.hpp"
 
+#include <cstring>
+#include <Windows.h>
+
 
 namespace Core
 {
@@ -8,13 +11,32 @@ namespace Core
         m_Address(address),
         m_Size(size)
     {
+        m_OriginalBytes = new uint8_t[size];
+    }
+
+    Patch::~Patch()
+    {
+        delete[] m_OriginalBytes;
     }
     
-    void Patch::Apply()
+    void Patch::Apply(const void* bytes)
     {
+        DWORD oldProtection = 0;
+        VirtualProtect(m_Address, m_Size, PAGE_EXECUTE_READWRITE, &oldProtection);
+
+        memcpy_s(m_OriginalBytes, m_Size, m_Address, m_Size);
+        memcpy_s(m_Address, m_Size, bytes, m_Size);
+
+        VirtualProtect(m_Address, m_Size, oldProtection, &oldProtection);
     }
     
     void Patch::Remove()
     {
+        DWORD oldProtection = 0;
+        VirtualProtect(m_Address, m_Size, PAGE_EXECUTE_READWRITE, &oldProtection);
+
+        memcpy_s(m_Address, m_Size, m_OriginalBytes, m_Size);
+
+        VirtualProtect(m_Address, m_Size, oldProtection, &oldProtection);
     }
 }
